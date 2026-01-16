e692faa8d486a668c86aa9ff8f4934566991e441
asrith-raju <asrithraju143@gmail.com>
2026-01-16 12:36:40 +0530

    server(feat): Create express server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+
+const app = express(); 
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+}))
+
+app.get('/', (req, res)=>{
+    res.send("API is running...");
+})
+app.listen(PORT, ()=>{
+    console.log(`Server started on PORT: ${PORT}`);
+})
