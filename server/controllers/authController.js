f838feaee8090fb64b90bdb858f5eb7e299fcae0
asrith-raju <asrithraju143@gmail.com>
2026-01-16 21:35:53 +0530

    server(feat): add user login controller function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -28,7 +28,38 @@ export const register = async (req,res)=>{
         sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
         maxAge:7*24*60*60*1000
         })
+        res.json({success:true})
     } catch (error) {
         res.json({success:false,message:error.message})
+    }
+}
+
+export const login = async (req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+        return res.json({success:false,message:"Missing Details"})
+    }
+    try {
+        const user = await userModel.findOne({email})
+        if(!user){
+            return res.json({success:false,message:"User Not Found"})   
+        }
+       const isMatch = await bcrypt.compare(password,user.password)
+
+       if(!isMatch){
+        return res.json({success:false,message:"Invalid Password"})   
+       }
+
+       const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'})
+
+       res.cookie('token',token,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV === 'production',
+        sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        maxAge:7*24*60*60*1000
+        })
+        return res.json({success:true,message:"Login Successful"})
+    } catch (error) {
+        return res.json({success:false,message:error.message})
     }
 }
