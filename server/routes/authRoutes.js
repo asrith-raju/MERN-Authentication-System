6ed6ed4615013c2d3d19f6a59fa3ad32e95646b2
asrith-raju <asrithraju143@gmail.com>
2026-01-17 21:35:43 +0530

    server(feat): Create route for logout controller function

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { login, register } from '../controllers/authController.js';
+import { login, logout, register } from '../controllers/authController.js';
 
 
 const authRouter = express.Router();
 
 authRouter.post('/register',register)
 authRouter.post('/login',login)
+authRouter.post('/logout',logout)
