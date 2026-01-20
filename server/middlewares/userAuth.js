f5487389ab04c48be39a8df2ebc16df7e0f670ff
asrith-raju <asrithraju143@gmail.com>
2026-01-20 10:30:00 +0530

    add userAuth middleware

---
+++ server/middlewares/userAuth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+
+const userAuth = (req,res,next)=>{
+    const {token} = req.cookies;
+
+    if(!token){
+        return res.status(401).json({success:false,message:"Not Unauthorized"})
+    }
+
+    try {
+        
+        const tokenDecode = jwt.verify(token,process.env.JWT_SECRET)
+        if(tokenDecode.id){
+            req.body.userId = tokenDecode.id 
+        }else{
+            return res.json({success:false,message:"not authorized Login again"})
+        }
+        next();
+    } catch (error) {
+        res.json({success:false,message:error.message})
+    }
+}
+
+export default userAuth;
