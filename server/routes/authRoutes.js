09d68a68a0c625c22de53a236b1c8cd91f6f4f60
asrith-raju <asrithraju143@gmail.com>
2026-01-21 10:30:00 +0530

    server(feat): Create route for sendVerifyotp controller function

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { login, logout, register } from '../controllers/authController.js';
+import { login, logout, register, sendVerifyOtp } from '../controllers/authController.js';
+import userAuth from '../middlewares/userAuth.js';
 
 
 const authRouter = express.Router();
@@ -7,5 +8,6 @@ const authRouter = express.Router();
 authRouter.post('/register',register)
 authRouter.post('/login',login)
 authRouter.post('/logout',logout)
+authRouter.post('/send-verify-otp',userAuth,sendVerifyOtp)
 
 export default authRouter
