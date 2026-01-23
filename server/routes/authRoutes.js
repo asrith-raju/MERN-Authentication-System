b18bae34c8dde2c4f5a9077bea5f4ed591faa6d5
asrith-raju <asrithraju143@gmail.com>
2026-01-23 21:21:53 +0530

    server(feat): Create route for sendResetOtp controller function

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { isAuthenticated, login, logout, register, sendVerifyOtp, verifyEmail } from '../controllers/authController.js';
+import { isAuthenticated, login, logout, register, resetPassword, sendResetOtp, sendVerifyOtp, verifyEmail } from '../controllers/authController.js';
 import userAuth from '../middlewares/userAuth.js';
 
 
@@ -11,6 +11,7 @@ authRouter.post('/logout',logout)
 authRouter.post('/send-verify-otp',userAuth,sendVerifyOtp)
 authRouter.post('/verify-account',userAuth,verifyEmail)
 authRouter.post('/is-auth',userAuth,isAuthenticated)
+authRouter.post('/send-reset-otp',sendResetOtp)
 
 
 export default authRouter
