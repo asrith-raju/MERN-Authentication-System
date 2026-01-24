a6aa54490c7209388bb6ccd7bdb52c023e226257
asrith-raju <asrithraju143@gmail.com>
2026-01-24 19:52:14 +0530

    client(feat): implement registration functionality in Login component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useState } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
 
 const Login = () => {
     const navigate = useNavigate()
+
+    const {backendUrl,setIsLoggedin} = useContext(AppContext)
+
     const [state, setState] = useState('Sign Up')
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+
+    const onSubmitHandler = async(e)=>{
+         try {
+            e.preventDefault();
+
+            axios.defaults.withCredentials = true
+            if(state === 'Sign Up'){
+               const {data} = await axios.post(backendUrl + '/api/auth/register',{
+                name,email,password
+               })
+               if(data.success){
+                setIsLoggedin(true)
+                navigate('/')
+               }
+            }else{
+               alert(data.message)
+            }
+         } catch (error) {
+            
+         }
+    }
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
         <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
@@ -16,7 +42,7 @@ const Login = () => {
             <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</h2>
             <p className='text-center text-sm mb-6'>{state === 'Sign Up' ? 'Create your account' : 'Login to your Account!'}</p>
 
-            <form>
+            <form onSubmit={onSubmitHandler}>
                 {state === 'Sign Up' && (<div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
                     <img src={assets.person_icon} alt="" />
                     <input onChange={e=>setName(e.target.value)} 
