e53d34b0b06c7c096df38a14f4b8e4f06b27e151
asrith-raju <asrithraju143@gmail.com>
2026-01-24 19:30:17 +0530

    client(feat): add state management for user input fields in Login component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,6 +5,9 @@ import { assets } from '../assets/assets'
 const Login = () => {
 
     const [state, setState] = useState('Sign Up')
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
         <img src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
@@ -15,17 +18,22 @@ const Login = () => {
             <form>
                 {state === 'Sign Up' && (<div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
                     <img src={assets.person_icon} alt="" />
-                    <input className='bg-transparent outline-none' type="text" placeholder='Full Name' required/>
+                    <input onChange={e=>setName(e.target.value)} 
+                    value={name} 
+                    className='bg-transparent outline-none' type="text" placeholder='Full Name' required/>
                 </div>)}
                 
                 <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
                     <img src={assets.mail_icon} alt="" />
-                    <input className='bg-transparent outline-none' type="email" placeholder='Email id' required/>
+                    <input onChange={e=>setEmail(e.target.value)} 
+                    value={email} 
+                    className='bg-transparent outline-none' type="email" placeholder='Email id' required/>
                 </div>
 
                 <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
                     <img src={assets.lock_icon} alt="" />
-                    <input className='bg-transparent outline-none' type="password" placeholder='Password' required/>
+                    <input onChange={e=>setPassword(e.target.value)} 
+                    value={password} className='bg-transparent outline-none' type="password" placeholder='Password' required/>
                 </div>
 
                 <p className='mb-4 text-indigo-500 cursor-pointer'>Forgot Password?</p>
