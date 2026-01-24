4c2eb01925a5c31c00f557f4bc82f95f371b5cc9
asrith-raju <asrithraju143@gmail.com>
2026-01-24 21:36:10 +0530

    client(feat): add user data fetching and state management in AppContext

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,14 +1,27 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useState,useEffect } from "react";
+import { toast } from "react-toastify";
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) =>{
    const backendUrl = import.meta.env.VITE_BACKEND_URL
    const [isLoggedin,setIsLoggedin] = useState(false)
-   const [userdata,setUserData] = useState(false)
+   const [userData,setUserData] = useState(false)
 
+   const getUserData = async () => {
+     try {
+        const {data} = await axios.get(backendUrl + '/api/user/data',  { withCredentials: true })
+        data.success ? setUserData(data.userData) : toast.error(data.message)
+     } catch (error) {
+        toast.error(error.message)
+     }
+   }
+     useEffect(() => {
+    getUserData();
+  }, []);
     const value = {
-        backendUrl,isLoggedin,setIsLoggedin,userdata,setUserData
+        backendUrl,isLoggedin,setIsLoggedin,userData,setUserData,getUserData
     }
     return(
         <AppContext.Provider value={value}>
