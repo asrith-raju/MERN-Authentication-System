07db3c940f2a36d8a8f262571394a6d4e4d66494
asrith-raju <asrithraju143@gmail.com>
2026-01-25 20:39:14 +0530

    client(feat): add corresponding functions for setting up new password

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,22 +3,27 @@ import { createContext, useState,useEffect } from "react";
 import { toast } from "react-toastify";
 
 export const AppContext = createContext()
+axios.defaults.withCredentials = true;
 
 export const AppContextProvider = (props) =>{
-  axios.defaults.withCredentials = true;
    const backendUrl = import.meta.env.VITE_BACKEND_URL
    const [isLoggedin,setIsLoggedin] = useState(false)
    const [userData,setUserData] = useState(false)
 
    const getAuthState = async () => {
       try {
-        const {data} = await axios.get( backendUrl + '/api/auth/is-auth')
+        const {data} = await axios.get( backendUrl + '/api/auth/is-auth',{ withCredentials: true })
         if(data.success){
           setIsLoggedin(true)
           getUserData()
         }
       } catch (error) {
-        toast.error(error.message)
+        if (error.response && error.response.status === 401) {
+                setIsLoggedin(false);
+                setUserData(false);
+            } else {
+                toast.error("Error checking auth state");
+            }
       }
    }
 
@@ -27,11 +32,12 @@ export const AppContextProvider = (props) =>{
         const {data} = await axios.get(backendUrl + '/api/user/data',  { withCredentials: true })
         data.success ? setUserData(data.userData) : toast.error(data.message)
      } catch (error) {
-        toast.error(error.message)
+         if (!(error.response && error.response.status === 401)) {
+                toast.error("Failed to load user data");
+            }
      }
    }
      useEffect(() => {
-    getUserData();
     getAuthState()
   }, []);
     const value = {
