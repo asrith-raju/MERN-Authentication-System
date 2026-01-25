ca5dded3c3fd421d089f421af17b15ca6c3ebb2f
asrith-raju <asrithraju143@gmail.com>
2026-01-25 18:52:15 +0530

    client(feat): add form for password submission

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -6,6 +6,9 @@ const ResetPassword = () => {
 
   const navigate = useNavigate()
   const [email, setEmail] = useState('')
+   const inputRefs = React.useRef([])
+
+  
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
        <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
@@ -20,6 +23,25 @@ const ResetPassword = () => {
           <button className='w-full py-2.5 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full mt-3'>Submit</button>
        </form>
      
+       {/* otp input for */}
+        
+        <form  className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+          <h1 className='text-white text-2xl font-semibold text-center mb-4'>Reset Password OTP</h1>
+          <p className='text-center mb-6 text-indigo-300'>Enter the 6-digit code sent to your email id.</p>
+          <div className='flex justify-between mb-8' onPaste={handlePaste}>
+            {Array(6).fill(0).map((_, index)=>(
+                <input type="text" maxLength={1} key={index} required
+                className='w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md'
+                ref={e=>inputRefs.current[index] = e}
+                onInput={(e)=> handleInput(e,index)}
+                onKeyDown={(e)=>handleKeyDown(e,index)}
+                />
+            ))}
+          </div>
+          <button className='w-full py-2.5 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full'>Verify email</button>
+      </form>
+
+
     </div>
   )
 }
