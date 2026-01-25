76108240d5f2982e63bec1c8b9f34fa23f6e51b2
asrith-raju <asrithraju143@gmail.com>
2026-01-25 13:13:16 +0530

    client(feat): implement input handling for OTP fields in EmailVerify component

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -3,6 +3,19 @@ import { assets } from '../assets/assets'
 
 
 const EmailVerify = () => {
+
+  const inputRefs = React.useRef([])
+
+  const handleInput = (e,index)=>{
+   if(e.target.value.length > 0 && index<inputRefs.current.length - 1){
+      inputRefs.current[index+1].focus()
+   }
+  }
+  const handleKeyDown = (e,index)=>{
+    if(e.key === 'Backspace' && e.target.value === '' && index >0){
+      inputRefs.current[index-1].focus()
+    }
+  }
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
       <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
@@ -13,6 +26,9 @@ const EmailVerify = () => {
             {Array(6).fill(0).map((_, index)=>(
                 <input type="text" maxLength={1} key={index} required
                 className='w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md'
+                ref={e=>inputRefs.current[index] = e}
+                onInput={(e)=> handleInput(e,index)}
+                onKeyDown={(e)=>handleKeyDown(e,index)}
                 />
             ))}
           </div>
