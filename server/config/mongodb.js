95b649dfa16134dae1bbf78bd5d086aff21b9023
asrith-raju <asrithraju143@gmail.com>
2026-04-05 10:30:00 +0530

    add inline documentation for mongodb file

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
+// Function to connect application with MongoDB database
 const connectDB = async () => {
+
+    // Event listener triggered when MongoDB connection is successful
     mongoose.connection.on("connected", () => {
         console.log("MongoDB connected successfully");
     })
+
+    // Connect to MongoDB database using connection URI
     await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`)
 }
 
+// Export database connection function
 export default connectDB;
