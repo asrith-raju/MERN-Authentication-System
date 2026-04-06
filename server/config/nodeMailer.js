c2d7ce410d3f91176f6b8cb2d1a89b2904eca302
asrith-raju <asrithraju143@gmail.com>
2026-04-06 10:30:00 +0530

    add inline documentation for nodeMailer file

--- server/config/nodeMailer.js
+++ server/config/nodeMailer.js
@@ -1,12 +1,20 @@
 import nodemailer from 'nodemailer'
 
+// Create transporter object for sending emails
 const transporter = nodemailer.createTransport({
+
+     // SMTP server host
      host:'smtp-relay.brevo.com',
+
+     // SMTP server port
      port:587,
+
+     // Authentication credentials for email service
      auth:{
           user:process.env.SMTP_USER,
           pass:process.env.SMTP_PASSWORD,
      }
 })
 
+// Export transporter for use in other files
 export default transporter
