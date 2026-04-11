656fedadb73510f2f9d6b10dc726e6bfd2332510
asrith-raju <asrithraju143@gmail.com>
2026-04-11 10:30:00 +0530

    client(docs): add inline documentation for Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 
+// Home page component
 const Home = () => {
+
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-center  bg-cover'>
+
+    // Main home page container
+    <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-center bg-cover'>
+
+     {/* Navigation bar */}
      <Navbar/>
+
+     {/* Header section */}
      <Header/>
+
     </div>
   )
 }
 
+// Export Home component
 export default Home
